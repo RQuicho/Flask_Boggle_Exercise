2a0ab1253ea075f70fd4930f62a989f89be23f0a
RQuicho <raymondquicho@gmail.com>
2023-05-02 17:32:48 -0700

    creating home route that displays a new board.

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from boggle import Boggle
+from flask import Flask, render_template, request, redirect, session, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my-secret-key'
 
 boggle_game = Boggle()
+
+@app.route('/')
+def home_page():
+	"""Creates a new board."""
+	board = boggle_game.make_board()
+
+	return render_template('make_board.html', board=board)
+
+
+
+
+
